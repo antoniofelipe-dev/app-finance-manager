d09f197055b3530a97e74463552e9dc6aef1e2e4
Antonio Felipe <antoniofelipii@gmail.com>
2023-06-05 23:49:56 -0300

    Fisrt Commit

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
+  const [expense, setExpense] = useState('');
+  const [expenses, setExpenses] = useState([]);
+  /* const [theme, setTheme] = useState('light'); */ 
+
+  const addExpense = () => {
+    if (expense) {
+      setExpenses([...expenses, parseFloat(expense)]);
+      setExpense('');
+    }
+  };
+
+  const calculateTotal = () => {
+    const total = expenses.reduce((accumulator, current) => accumulator + current, 0);
+    alert(`Total de gastos: R$ ${total.toFixed(2)}`);
+  };
+
+  const resetExpenses = () => {
+    setExpenses([]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Gerenciador de Gastos</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Digite o seu gasto"
+        value={expense}
+        onChangeText={(text) => setExpense(text)}
+        keyboardType="numeric"
+      />
+      <Button title="Adicionar" onPress={addExpense} />
+      <View style={styles.buttonsContainer}>
+        <Button title="Somar Gastos" onPress={calculateTotal} />
+        <Button title="Zerar Gastos" onPress={resetExpenses} />
+      </View>
+      <FlatList
+        style={styles.expenseList}
+        data={expenses}
+        renderItem={({ item }) => <Text style={styles.expenseItem}>{item.toFixed(2)}</Text>}
+        keyExtractor={(_, index) => index.toString()}
+      />
     </View>
   );
 }
@@ -13,8 +50,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    padding: 40,
+    marginTop: 100
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center'
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  expenseList: {
+    marginTop: 20,
+  },
+  expenseItem: {
+    marginBottom: 10,
+    fontSize: 16,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 20,
   },
 });
